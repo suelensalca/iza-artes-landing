6c4faf36f030bc290530f7e9aab4c9301bb15956
Suelen <suelen.salca@ringa.com.br>
2025-04-30 23:56:39 -0300

    Add mui, eslint and prettier with configs

---
+++ styles/theme.ts
@@ -0,0 +1,15 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto',
+    fontSize: 30,
+  },
+});
+
+export default theme;
