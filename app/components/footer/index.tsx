b136653f704883460ecbf8f4dd13ba5246ffbc40
Suelen <suelen.salca@ringa.com.br>
2025-05-30 23:45:45 -0300

    Add mui icons and change footer info

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,17 +1,30 @@
 'use client';
 
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import LocationPinIcon from '@mui/icons-material/LocationPin';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 import { Box, Typography } from '@mui/material';
 import Link from 'next/link';
-import { Container, FlexSpaceBetween, Info, NavigationButton, Section } from './styles';
+import {
+  CleanLink,
+  Container,
+  FlexSpaceBetween,
+  Info,
+  NavigationButton,
+  Section,
+  SmallIcon,
+  Title,
+} from './styles';
 
 export default function Footer() {
   return (
     <Container>
       <FlexSpaceBetween>
         <Section>
-          <Typography sx={{ fontSize: '21px', fontWeight: '500', color: 'white' }}>
-            Navegação
-          </Typography>
+          <Title>Navegação</Title>
           <Link href="/">
             <NavigationButton disableTouchRipple>Página Inicial</NavigationButton>
           </Link>
@@ -26,20 +39,39 @@ export default function Footer() {
           </Link>
         </Section>
         <Section>
-          <Typography sx={{ fontSize: '21px', fontWeight: '500', color: 'white' }}>
-            Contato
-          </Typography>
-          <Info>(41) 99999-9999</Info>
-          <Info>@izamartinelli.artes</Info>
-          <Info>Av. Sete de Setembro, 1865 - Centro, Curitiba - PR, BOX 304</Info>
+          <Title>Contato</Title>
+          <Info>
+            <LocalPhoneIcon sx={SmallIcon} />
+            (41) 99999-9999
+          </Info>
+          <Info>
+            <EmailIcon sx={SmallIcon} />
+            iza_martinelli@hotmail.com
+          </Info>
+          <Info sx={{ alignItems: 'flex-start' }}>
+            <LocationPinIcon sx={SmallIcon} />
+            Av. Sete de Setembro, 1865 - Centro, Curitiba - PR, BOX 304
+          </Info>
         </Section>
         <Section>
-          <Typography sx={{ fontSize: '21px', fontWeight: '500', color: 'white' }}>
-            Redes Sociais
-          </Typography>
-          <Info>insta</Info>
-          <Info>wpp</Info>
-          <Info>email</Info>
+          <Title>Redes Sociais</Title>
+          <Box sx={{ color: 'white', display: 'flex', gap: '20px' }}>
+            <CleanLink
+              href="https://www.instagram.com/izamartinelli.artes"
+              target="_blank"
+              rel="noopener noreferrer">
+              <InstagramIcon sx={{ fontSize: { xs: 36, sm: 45 } }} />
+            </CleanLink>
+            <CleanLink href="https://wa.me/5541999999999" target="_blank" rel="noopener noreferrer">
+              <WhatsAppIcon sx={{ fontSize: { xs: 36, sm: 45 } }} />
+            </CleanLink>
+            <CleanLink
+              href="mailto:iza_martinelli@hotmail.com"
+              target="_blank"
+              rel="noopener noreferrer">
+              <EmailIcon sx={{ fontSize: { xs: 36, sm: 45 } }} />
+            </CleanLink>
+          </Box>
         </Section>
       </FlexSpaceBetween>
       <Box>
