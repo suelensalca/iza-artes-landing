f8dc014f6c45048271359a3d59295f989806caeb
Suelen <suelen.salca@ringa.com.br>
2025-06-01 12:45:27 -0300

    Add about constants and images

---
+++ app/(pages)/about/constants.tsx
@@ -0,0 +1,13 @@
+export const Constants = {
+  title: 'Iza Artes',
+  textIntro:
+    'Iza Artes nasceu do amor pelas artes manuais e da vontade de transformar criatividade em peças únicas, feitas com afeto e carregadas de significado especial. Há 25 anos Iza produz trabalhos de artesanato, com experiência em bordado, tricô, crochê, costura e patchwork.',
+  textHandmade:
+    'Cada peça é resultado de um processo cuidadoso e artesanal, onde a atenção aos detalhes faz toda a diferença. O trabalho manual permite uma liberdade criativa que vai além da estética: ele traduz histórias, gostos e sentimentos em produtos exclusivos. As encomendas personalizadas são desenvolvidas com base nas preferências de cada cliente, levando em conta cores, estilos, funcionalidades e até lembranças afetivas. Assim, surgem criações que combinam beleza, utilidade e emoção.',
+  textProducts:
+    'Entre os principais produtos estão necessaires, bolsas, carteiras, ecobags, porta óculos, panos de prato e itens de cozinha. Tudo é feito à mão, com tecidos escolhidos a dedo, acabamentos caprichados e um olhar especial para a harmonia entre materiais e composição. As peças também podem incluir bordados personalizados, com nomes, logos ou imagens.',
+  textMarket:
+    'Além das encomendas, parte da produção da Iza Artes está disponível à pronta entrega no Box 304 do Mercado Municipal de Curitiba, um espaço onde a criatividade de diversos artesãos se encontra. Faça uma visita e conheça de perto produções autorais de cerâmica, tecido, pintura e muito mais!',
+  textEnd:
+    'Mais do que uma marca, a Iza Artes representa uma história de carinho e dedicação pelo feito à mão.',
+};
--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,7 +1,62 @@
 'use client';
 
-import { Box } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import Image from 'next/image';
+import { Constants } from './constants';
+import atelieMain from '../../../public/assets/intro/atelie9.webp';
+import atelie1 from '../../../public/assets/intro/atelie1.webp';
+import atelie2 from '../../../public/assets/intro/atelie2.webp';
+import atelie3 from '../../../public/assets/intro/atelie3.webp';
+import atelie5 from '../../../public/assets/intro/atelie5.webp';
+import atelie6 from '../../../public/assets/intro/atelie6.webp';
+import atelie8 from '../../../public/assets/intro/atelie8.webp';
 
 export default function About() {
-  return <Box>Sobre</Box>;
+  return (
+    <Grid container my={8} mx={{ xs: 4, xxl: 10 }} spacing={4}>
+      <Grid size={{ xs: 12, md: 7 }}>
+        <Typography fontWeight={600} color="secondary.main">
+          {Constants.title}
+        </Typography>
+        <Typography fontStyle="italic" fontSize={16} mb={2}>
+          {Constants.textIntro}
+        </Typography>
+        <Typography fontStyle="italic" fontSize={16} mb={2}>
+          {Constants.textHandmade}
+        </Typography>
+        <Typography fontStyle="italic" fontSize={16} mb={2}>
+          {Constants.textProducts}
+        </Typography>
+        <Typography fontStyle="italic" fontSize={16} mb={2}>
+          {Constants.textMarket}
+        </Typography>
+        <Typography fontStyle="italic" fontSize={16} mb={2}>
+          {Constants.textEnd}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid size={4} position="relative" minHeight={200}>
+            <Image src={atelie8} alt="maquina" fill style={{ objectFit: 'cover' }} />
+          </Grid>
+          <Grid size={4} position="relative" minHeight={200}>
+            <Image src={atelie1} alt="balcao" fill style={{ objectFit: 'cover' }} />
+          </Grid>
+          <Grid size={4} position="relative" minHeight={200}>
+            <Image src={atelie2} alt="bordado" fill style={{ objectFit: 'cover' }} />
+          </Grid>
+          <Grid size={4} position="relative" minHeight={200}>
+            <Image src={atelie3} alt="maquina" fill style={{ objectFit: 'cover' }} />
+          </Grid>
+          <Grid size={4} position="relative" minHeight={200}>
+            <Image src={atelie6} alt="balcao" fill style={{ objectFit: 'cover' }} />
+          </Grid>
+          <Grid size={4} position="relative" minHeight={200}>
+            <Image src={atelie5} alt="bordado" fill style={{ objectFit: 'cover' }} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid size={{ xs: 12, md: 5 }} position="relative" minHeight={{ xs: 600, md: 400 }}>
+        <Image src={atelieMain} alt="iza costurando" fill style={{ objectFit: 'cover' }} />
+      </Grid>
+    </Grid>
+  );
 }
